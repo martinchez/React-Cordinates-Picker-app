6c7a578f95ef8f9b33df45d0ec3360b84074cad8
martinchez <martinchege1998@gmail.com>
2024-06-14 17:28:53 +0300

    adding android and ios build application

--- src/App.js
+++ src/App.js
@@ -30,11 +30,11 @@ const MapComponent = () => {
     // Create custom icon for marker
     const customMarkerIcon = L.icon({
       iconUrl: markerIconUrl,
-      iconSize: [38, 95], // size of the icon
-      shadowSize: [50, 64], // size of the shadow
-      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+       iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     })
 
     // Function to convert coordinates to address
@@ -277,6 +277,9 @@ const MapComponent = () => {
           />
         </div>
       </div>
+      <p>
+        Total Visitors: <span id="visitorCount">0</span>
+      </p>
     </div>
   )
 }
